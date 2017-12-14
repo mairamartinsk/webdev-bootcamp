0ae6afd91872184f50dbc9ee1129af697494d2e6
Maira Martins K <mairamartinsk@gmail.com>
2017-12-14 17:35:20 +0100

    Add Semantic UI and initial app styling

--- blog-app/app.js
+++ blog-app/app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 var blogSchema = new mongoose.Schema({
   title: String,
   image: String,
-  text: String,
+  body: String,
   created: {type: Date, default: Date.now}
 });
 
@@ -24,6 +24,22 @@ var Blog = mongoose.model('Blog', blogSchema);
 
 // RESTFUL ROUTES
 
+app.get('/', function(req, res){
+  res.redirect('/blogs');
+});
+
+app.get('/blogs', function(req, res){
+  Blog.find({}, function(error, blogs){
+    if (error) {
+      console.log(error);
+    } else {
+      res.render('index', {blogs: blogs});
+    }
+  });
+});
+
+
+// START SERVER
 
 app.listen(3000, function(){
   console.log('Listening on port 3000');
